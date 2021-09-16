8ac9b71e1d500b2284be3245142155c8f2a1c301
شهد محمد <90135788+shashaNEKO@users.noreply.github.com>
2021-09-16 20:45:00 +0300

    final

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
     }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent();
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+
+
+    TextView textView = findViewById(R.id.textView);
+        textView.setText(message); }
 }
